d71de6fd4181576d369d6545a9d698e7e266adc9
Braden Walters <vc@braden-walters.info>
2019-01-06 16:34:18 +0100

    Flower spawn point for bees

---
+++ Assets/Scripts/Bee/FlowerSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Bee/FlowerSpawn")]
+public class FlowerSpawn : MonoBehaviour {
+  public int activeBeeLimit = 3;
+  public float minNextBeeSeconds = 10f;
+  public float maxNextBeeSeconds = 30f;
+
+  [SerializeField] private GameObject beePrefab;
+  [SerializeField] private Transform spawnPoint;
+
+  private float untilNextBee;
+  private List<GameObject> activeBees = new List<GameObject>();
+
+  private float RandNextBeeDelay {
+    get { return Random.Range(this.minNextBeeSeconds, this.maxNextBeeSeconds); }
+  }
+
+  void Start() {
+    this.untilNextBee = this.RandNextBeeDelay;
+  }
+
+  void Update() {
+    this.activeBees.RemoveAll(bee => bee == null);
+    this.untilNextBee -= Time.deltaTime;
+
+    if (untilNextBee < 0f) {
+      if (this.activeBees.Count < this.activeBeeLimit) {
+        var bee = GameObject.Instantiate(
+          this.beePrefab,
+          this.spawnPoint.transform.position,
+          Quaternion.Euler(0f, Random.Range(0f, 360), 0f));
+        this.activeBees.Add(bee);
+      }
+
+      this.untilNextBee = this.RandNextBeeDelay;
+    }
+  }
+}
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [AddComponentMenu("UI/PauseMenu")]
 public class PauseMenu : MonoBehaviour {
