62d1f3eb9c6a2815b5f2dc18d7cf862ed02a07fd
Braden Walters <vc@braden-walters.info>
2019-01-17 21:31:50 +0100

    Fish collectible

---
+++ Assets/Scripts/Fish/FishCollectible.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Fish/FishCollectible")]
+public class FishCollectible : MonoBehaviour {
+  private float rotY = 0f;
+
+  void Update() {
+      this.rotY += Time.deltaTime;
+      this.transform.Rotate(new Vector3(0f, this.rotY, 0f));
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.GetComponent<SingletonPlayer>() != null) {
+      PlayerManager.Current.CountFish();
+      Debug.Log("Fish collect: "
+        + PlayerManager.Current.FishCount + " / "
+        + PlayerManager.Current.TotalFishCount);
+      Destroy(this.gameObject);
+    }
+  }
+}
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -3,15 +3,19 @@
 public class PlayerManager : BaseManager {
   public const string IsPausedEvent = "PlayerManager.IsPaused";
   public const string BeeCountEvent = "PlayerManager.BeeCountEvent";
+  public const string FishCountEvent = "PlayerManager.FishCountEvent";
 
   public static PlayerManager Current { get; private set; }
 
   public GameObject PlayerObject { get; private set; }
   public bool IsPaused { get; private set; }
   public int BeeCount { get; private set; }
+  public int TotalFishCount { get; private set; }
+  public int FishCount { get; private set; }
 
   public override void Start() {
     this.BeeCount = 0;
+    this.TotalFishCount = FindObjectsOfType<FishCollectible>().Length;
     Current = this;
     this.Ready = true;
   }
@@ -32,5 +36,10 @@ public class PlayerManager : BaseManager {
   public void CountBee() {
     this.BeeCount += 1;
     EventSystem.Publish(this, BeeCountEvent);
+  }
+
+  public void CountFish() {
+    this.FishCount += 1;
+    EventSystem.Publish(this, FishCountEvent);
   }
 }
