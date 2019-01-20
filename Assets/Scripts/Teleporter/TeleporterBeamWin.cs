0b1ce17f0c8f8f525bca2dd3c993f0cd46fa7cc5
Braden Walters <vc@braden-walters.info>
2019-01-20 23:50:44 +0100

    Teleporter to end game that is activated after collecting all fish

---
+++ Assets/Scripts/Teleporter/TeleporterBeamWin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Teleporter/TeleporterBeamWin")]
+public class TeleporterBeamWin : MonoBehaviour {
+  void OnTriggerEnter(Collider other) {
+    if (other.GetComponent<SingletonPlayer>() != null) {
+      Debug.Log("You win!");
+    }
+  }
+}
---
+++ Assets/Scripts/Teleporter/TeleporterSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[AddComponentMenu("Teleporter/TeleporterSwitch")]
+public class TeleporterSwitch : MonoBehaviour {
+  [SerializeField] GameObject beam;
+
+  void Start() {
+    this.beam.SetActive(false);
+    EventSystem.Subscribe(PlayerManager.FishCountEvent, this.TryEnable);
+  }
+
+  void OnDestroy() {
+    EventSystem.Unsubscribe(PlayerManager.FishCountEvent, this.TryEnable);
+  }
+
+  private void TryEnable(object sender, EventArgs args) {
+    var playerManager = PlayerManager.Current;
+    if (playerManager.FishCount == playerManager.TotalFishCount) {
+      this.beam.SetActive(true);
+    }
+  }
+}
